bbd484a4e792105b68357c88d99e90ed9ecc8e87
Dalton Mills <mills.dalton@gmail.com>
2021-09-26 22:27:58 -0500

    fix header padding

--- src/layout/PageHeader.tsx
+++ src/layout/PageHeader.tsx
@@ -6,7 +6,7 @@ interface PageHeaderProps {
 }
 
 export const PageHeader = ({ title, icon }: PageHeaderProps) => (
-    <Box bg="canvas.inset" boxShadow="shadow.medium">
+    <Box bg="canvas.inset" boxShadow="shadow.small">
         <Heading fontSize={3} fontWeight={600} p={4}>
             {icon && <StyledOcticon icon={icon} size={24} mr={1} />}
             <span>{title}</span>
