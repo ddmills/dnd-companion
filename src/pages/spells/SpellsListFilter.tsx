df1941d0ed253748e23e79be9d6af5b80db01955
Dalton Mills <mills.dalton@gmail.com>
2021-09-29 21:34:43 -0500

    Render classnames in filter list

--- src/pages/spells/SpellsListFilter.tsx
+++ src/pages/spells/SpellsListFilter.tsx
@@ -23,6 +23,25 @@ const CloseButton = styled(UnstyledButton)`
     }
 `;
 
+
+const ClassGrid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-column-gap: 8px;
+    height: 320px;
+    grid-row-gap: 8px;
+    padding: 8px;
+`;
+
+const ClassCell = styled(UnstyledButton)`
+    background-color: ${themeGet('colors.canvas.subtle')};
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
 interface SpellsListFilterProps {
     handleClose: () => void;
 }
@@ -36,6 +55,19 @@ export const SpellsListFilter = ({ handleClose }: SpellsListFilterProps) => {
                     <StyledOcticon icon={XIcon} size={32} />
                 </CloseButton>
             </TitleBar>
+            <ClassGrid>
+                <ClassCell>Barbarian</ClassCell>
+                <ClassCell>Bard</ClassCell>
+                <ClassCell>Cleric</ClassCell>
+                <ClassCell>Druid</ClassCell>
+                <ClassCell>Fighter</ClassCell>
+                <ClassCell>Monk</ClassCell>
+                <ClassCell>Paladin</ClassCell>
+                <ClassCell>Ranger</ClassCell>
+                <ClassCell>Sorcerer</ClassCell>
+                <ClassCell>Warlock</ClassCell>
+                <ClassCell>Wizard</ClassCell>
+            </ClassGrid>
         </Box>
     );
 };
